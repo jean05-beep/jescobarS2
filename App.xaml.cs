7f0d12b7c6ddb830a1668c0c228fe17debce391f
jean0 <jean0@LAPTOP-83UKQI1M>
2024-10-26 14:09:19 -0500

    Agregar archivos de proyecto.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using jescobarS2.Views;
+
+namespace jescobarS2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Estudiante();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace jescobarS2
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
